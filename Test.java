952c1a5003d03fd490a6c72d6d7430680193147d
RahulVarunGit <rahulvarun.b@gmail.com>
2018-02-17 13:12:27 -0600

    updated comment

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@
 public class Test {
   public static void main(String args[])
   {
-    System.out.println("***************** test spring mvc **************** ");
+    System.out.println("***************** test spring mvc edited **************** ");
   }
 }
